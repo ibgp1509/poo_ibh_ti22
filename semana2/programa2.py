d9f74af0bc3aec6d0679aa433fdb6b1bb828a75c
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-01-24 14:06:14 +0000

    Distinta formas de concatenar variables, así como la sintaxis de cada forma y la manera correcta de escribir sin dar error

---
+++ semana2/programa2.py
@@ -0,0 +1,14 @@
+"""
+    Programa2
+    Nombre:Cesar Ivan Bustamante Herrera
+    Fecha:24/01/2023
+    Descripcion; Distinta formas de concatenar variables, asi como la sintaxis de cada forma y la manera correcta de escribir sin dar error
+"""
+variable1= "Hola"  # esta definiendo una variable de tipo string o texto
+variable2= 619
+# variable estring
+#print(variable1 + variable2)  #concatenar variables de forma simple
+
+print("{} {}".format(variable1 , variable2)) #otra forma  de escribir la concatenacion
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                              
