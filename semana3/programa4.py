6444510f85184796a2e108e942a4d416fc16c200
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-02-01 14:01:37 +0000

    operaciones matematicas

---
+++ semana3/programa4.py
@@ -0,0 +1,21 @@
+"""
+    Programa4
+    Nombre:Cesar Ivan Bustamante 
+    Herrera
+    Fecha:30/01/2023
+    Descripcion; Cambiar tipo de dato 
+    casting
+"""
+numero1=input("Numero1: ")
+numero2= 10
+suma= int(numero1)+ numero2
+resta= int(numero1)- numero2
+multiplicacion=int(numero1) * numero2
+division= int(numero1) / numero2
+potencia=int(numero1) ^ numero2
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(potencia)
---
+++ semana3/programa6.py
@@ -0,0 +1,31 @@
+"""
+    Programa6
+    Nombre:Cesar Ivan Bustamante 
+    Herrera
+    Fecha:31/01/2023
+    Descripcion; Calcular el area de 
+    un circulo
+"""
+print("--------AREA DEL CIRCULO----------")# Este es un separador para que el resultado no se vea  junto
+
+pi=(3.1416)
+radio=float(input("Escribe el valor del radio"))
+
+area_circulo=pi*(radio ** 2)
+print("El area del circulo con un radio de:{} es {}".format(radio, area_circulo))
+
+print("--------PERIMETRO DEL CIRCULO----------")# Este es un separador para que el resultado no se vea junto
+
+diametro=float(input("Escribe el diamtero"))
+perimetro=(diametro * pi)
+print("El perimetro del circulo es:", perimetro)
+
+
+
+print("-----------AREA DEL CUADRADO----------------")# Este es un separador parea que el resultado no se vea todo junto
+
+lado=float(input("Ingresa el lado ")) # Esta es otra forma con la funcion input, que te permite ingresar datos desde el teclado y ponerlas en uso ya que almacena y guarda los datos en la misma,  de primero se ingresa el : nombre de la variable = tipo de dato (input ("la indicacion")) 
+
+
+
+print( " El area es de: " , lado * lado ) # Aqui estamos imprimiendo y ejecutanto el valor de lo que queremos saber, en este caso el area 
