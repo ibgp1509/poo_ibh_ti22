44fb6421893b30238f1cc296d87f560cb4aedc8a
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-02-07 04:58:23 +0000

    Mi primer repaso de lo aprendido. despues de varios problemas aqui seguimos

---
+++ avance_febrero/prueba1.py
@@ -0,0 +1,10 @@
+"""
+    Programa1
+    Nombre:Cesar Ivan Bustamante Herrera
+    Fecha:06/02/2023
+    Descripcion; cometarios multilinea, 
+    una linea e impresion de variables
+ """
+
+variable1= "Hola, Mi nombre es: Cesar Ivan Bustamante Herrera"  #  variable para almacenar una cadena de caracter y que se mostratara en pantalla
+print(variable1)  # Mostrara lo que contiene la variable
