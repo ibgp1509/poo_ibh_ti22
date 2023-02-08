52da7cf7087b913269f137685bcdf573ddde900b
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-02-08 04:11:49 +0000

    practica del como hacer y realizar una suma

---
+++ febrero/prueba2.py
@@ -0,0 +1,10 @@
+"""
+    Programa2
+    Nombre:Cesar Ivan Bustamante Herrera
+    Fecha:07/02/2023
+    Descripcion;suma
+ """
+
+num1= 20
+num2= 26
+print("La suma de estos numeros es: ", num1 + num2) # Es la indicacion para imprimir y realizar suma de los numeros
