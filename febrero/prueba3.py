a40ae8b84eb33d8eb97da88a9347769ba444f93d
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-02-09 03:57:26 +0000

    se creo un programa que pide 2 numeros de teclado y los multiplica

---
+++ febrero/prueba3.py
@@ -0,0 +1,11 @@
+"""
+    Programa3
+    Nombre:Cesar Ivan Bustamante Herrera
+    Fecha:08/02/2023
+    Descripcion; Multiplicacion con datos de teclado
+ """
+
+num1=float(input("Escribe el primer numero: "))
+
+num2=float(input("Escribe el segundo numero: "))
+print("La multiplicacion de ambos es: ", num1 * num2)
