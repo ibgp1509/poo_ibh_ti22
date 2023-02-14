84f083241b5cec84fed07ac3fdeaa45d86f6b1c6
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-02-14 05:28:32 +0000

    Hicimos 11 pruebas sobre las deciciones con if

---
+++ semana4/programa8.py
@@ -0,0 +1,117 @@
+"""
+    Programa8 -
+    Nombre:Cesar Ivan Bustamante 
+    Herrera
+    Fecha:08/02/2023
+    Descripcion; 11 pruebaas
+"""
+n1=int(input("N1"))
+n2=int(input("N2"))
+
+if n1 > n2 :
+  print (n1)
+if n2 > n1 :
+  print (n2)
+if n1 ==n2:
+  print(None)
+
+
+if n2 > n1 :
+  print (n2)
+if n1 > n2 :
+  print (n1)
+if n2== n1 :
+  print (None)
+
+
+if n1 > n2 :
+ print (n1)
+elif n2 > n1 :
+ print (n2 )
+else :
+ print (None) 
+
+
+if n1 < n2 :
+ print (n2)
+elif n2 < n1 :
+ print(n1)
+else:
+ print(None)
+
+
+if n2 < n1 :
+  print (n1)
+if n1 < n2 :
+  print (n2)
+if n1== n2 :
+  print (None)
+
+
+if n1 >= n2 :
+ if n1 > n2 :
+    print (n1)
+ else :
+     print (None)
+else:
+  print (n2) 
+
+
+if n1 <= n2 :
+  if n1 < n2 :
+    print(n2)
+else:
+    print (None)
+ else:
+    print (n1)
+
+
+if n1 <= n2
+ if n1== n2 :
+  
+   print (n2)
+else:
+ print (None)
+else:
+print (n1)
+
+
+if n1 == n2:
+  print (None)
+elif n1 > n2:
+print (n1)
+else:
+print (n2)
+
+
+if n1 == n2 :
+  print (None)
+elif n1 < n2 :
+print (n2)
+else:
+print (n1)
+
+
+if n1 == n2 :
+  print (None)
+
+elif n1 >= n2 :
+print (n1)
+
+else: 
+print (n2)
+
+
+ 
+
+
+
+
+
+
+  
+
+  
+
+
+    
