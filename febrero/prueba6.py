eba92dc6ad40ee814655f8bad7fb8f96f92635f2
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-02-14 18:51:12 +0000

    programa  para calcular edad

---
+++ febrero/prueba6.py
@@ -0,0 +1,10 @@
+"""
+    Programa6
+    Nombre:Cesar Ivan Bustamante Herrera
+    Fecha:14/02/2023
+    Descripcion; Calcular edad de una persona
+ """
+nac=int(input("Escribe tu año de nacimiento.... Los 4 digitos: "))
+añop=int(input("Escribe en el año pque te encuentras: "))
+
+print("De acuerdo con tus datos ingresado tienes o cumpliras: ", añop - nac)
