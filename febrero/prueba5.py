6f71840d79b74f19f87448609d5dede6b82267c6
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-02-14 04:53:00 +0000

    Programa que ayuda a dividir 3 numeros pedidos de teclado

---
+++ febrero/prueba5.py
@@ -0,0 +1,13 @@
+"""
+    Programa5
+    Nombre:Cesar Ivan Bustamante Herrera
+    Fecha:13/02/2023
+    Descripcion; Division de 3 numeros pedidos desde teclado
+ """
+
+n1=float(input("Escribe el primer numero: "))  # Estamos declarando variable de tipo flotante para que el programa reciba cualquier tipo de numero
+
+n2=float(input("Escribe el segundo numero: "))
+n3=float(input("Escribe el tercer numero: ")) # Esta es u8na variable que pide y almacena el tercer numero el cual se dividira los 2 anteriores
+
+print("La multiplicacion de los tres numeros es: ", n1 / n2 / n3)
