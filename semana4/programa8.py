ced5579c3032c8e4742cbe8ec50233d50856289f
ivanbgp1509 <122800689+ibgp1509@users.noreply.github.com>
2023-02-16 04:21:14 +0000

    Comente todas las  lineas del programa

--- semana4/programa8.py
+++ semana4/programa8.py
@@ -2,104 +2,151 @@
     Programa8 -
     Nombre:Cesar Ivan Bustamante 
     Herrera
-    Fecha:08/02/2023
+    Fecha:07/02/2023
     Descripcion; 11 pruebaas
 """
-n1=int(input("N1"))
-n2=int(input("N2"))
+n1=int(input("Escribe el primer numero: ")) # Se crea una variable de tipo entero para pedir datos de teclado, esto se logra con inpút
+n2=int(input("Escribe el segundo numero: ")) # Se crea una segunda variable para pedir datos de teclado con input
 
-if n1 > n2 :
-  print (n1)
-if n2 > n1 :
-  print (n2)
-if n1 ==n2:
+print("El numero mayor en las diferentes versiones es:") # Imprimira la leyenda que esta escrita entre parentesis
+
+print("Primer version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
+if n1 > n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
+  print (n1) # Se esta imprimiendo el resultado de las comparaciones 
+
+if n2 > n1 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
+  print (n2) # Se esta imprimiendo el resultado de las comparaciones
+
+if n1 ==n2:  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+ 
   print(None)
 
+mayor=(n1,n2)
 
-if n2 > n1 :
-  print (n2)
-if n1 > n2 :
-  print (n1)
-if n2== n1 :
+print("Segunda  version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
+
+if n2 > n1 :   # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
+  print (n2) # Se esta imprimiendo el resultado de las comparaciones
+
+if n1 > n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
+  print (n1) # Se esta imprimiendo el resultado de las comparaciones
+
+if n2== n1 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
   print (None)
 
+print("Tercer version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
 
-if n1 > n2 :
- print (n1)
-elif n2 > n1 :
- print (n2 )
+if n1 > n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+ 
+  print (n1) # Se esta imprimiendo el resultado de las comparaciones
+
+elif n2 > n1 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+ 
+  print (n2 ) # Se esta imprimiendo el resultado de las comparaciones
 else :
  print (None) 
 
+print("Cuarta version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
 
-if n1 < n2 :
- print (n2)
-elif n2 < n1 :
- print(n1)
-else:
+if n1 < n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+ 
+  print (n2) # Se esta imprimiendo el resultado de las comparaciones
+
+elif n2 < n1 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+
+  print(n1) # Se esta imprimiendo el resultado de las comparaciones
+else:   # Indica que si no se cumplen alguna de las condiciones anteriores de otro resultado
  print(None)
 
+print("Quinta version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
 
-if n2 < n1 :
-  print (n1)
-if n1 < n2 :
-  print (n2)
-if n1== n2 :
-  print (None)
-
-
-if n1 >= n2 :
- if n1 > n2 :
-    print (n1)
- else :
-     print (None)
-else:
-  print (n2) 
-
-
-if n1 <= n2 :
-  if n1 < n2 :
-    print(n2)
-else:
-    print (None)
- else:
-    print (n1)
-
-
-if n1 <= n2
- if n1== n2 :
+if n2 < n1 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
   
-   print (n2)
-else:
- print (None)
-else:
-print (n1)
+  print (n1) # Se esta imprimiendo el resultado de las comparaciones
 
+if n1 < n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+ 
+  print (n2) # Se esta imprimiendo el resultado de las comparaciones
 
-if n1 == n2:
+if n1== n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+ 
+  print (None) # Se esta imprimiendo el resultado de las comparaciones
+
+print("Sexta version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
+
+if n1 >= n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+ 
+  if n1 > n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+   
+    print (n1) # Se esta imprimiendo el resultado de las comparaciones
+ 
+  # Indica que si no se cumplen alguna de las condiciones anteriores de otro resultado
+  else :
+    print (None)
+else:
+      print (n2)  # Se esta imprimiendo el resultado de las comparaciones
+
+print("Septima version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
+
+if n1 <= n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
+  if n1 < n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+    
+    print(n2) # Se esta imprimiendo el resultado de las comparaciones
+ 
+  else:
+    print (None)
+else:
+ print (n1) # Se esta imprimiendo el resultado de las comparaciones
+
+print("Octava version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
+
+if n1 <= n2:  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
+  if n1== n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+     print (None)
+     
+  else:
+     print (n2) # Se esta imprimiendo el resultado de las comparaciones
+else:
+   print (n1) # Se esta imprimiendo el resultado de las comparaciones
+
+print("Novena version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
+if n1 == n2:  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
   print (None)
-elif n1 > n2:
-print (n1)
+elif n1 > n2:  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
+  print (n1) # Se esta imprimiendo el resultado de las comparaciones
 else:
-print (n2)
+  print (n2) # Se esta imprimiendo el resultado de las comparaciones
 
+print("Decima version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
 
-if n1 == n2 :
-  print (None)
-elif n1 < n2 :
-print (n2)
-else:
-print (n1)
-
-
-if n1 == n2 :
+if n1 == n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
   print (None)
 
-elif n1 >= n2 :
-print (n1)
+elif n1 < n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+ 
+  print (n2) # Se esta imprimiendo el resultado de las comparaciones
+else:
+  print (n1) # Se esta imprimiendo el resultado de las comparaciones
+
+print("Onceava version ") # Imprimira la leyenda que esta escrita entre parentesis, inicando que version es 
+
+if n1 == n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  print (None)
+
+elif n1 >= n2 :  # Condicion que esta haciendo una comparacion entre 2 variables, sea o no cierto la condicion se realiozara una accion
+  
+  print (n1) # Se esta imprimiendo el resultado de las comparaciones
 
 else: 
-print (n2)
+  print (n2) # Si no se cumple las condiciones anteriores el resultado tomara este valor 
 
 
  
